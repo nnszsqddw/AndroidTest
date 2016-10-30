29dd22155324114aa2832c0d87a51e580780da18
koinhug@126.com <koinhug@126.com>
2016-10-30 13:04:35 +0800

    动态计算状态栏高度

--- immersivestatusbartest/src/main/java/com/xorange/immersivestatusbar/SecondActivity.java
+++ immersivestatusbartest/src/main/java/com/xorange/immersivestatusbar/SecondActivity.java
@@ -1,7 +1,51 @@
 package com.xorange.immersivestatusbar;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Field;
 
 
 public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        initState();
+    }
+
+    private void initState() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            LinearLayout linear_bar = (LinearLayout) findViewById(R.id.ll_bar);
+            linear_bar.setVisibility(View.VISIBLE);
+            int statusHeight = getStatusBarHeight();
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) linear_bar.getLayoutParams();
+            params.height = statusHeight;
+            linear_bar.setLayoutParams(params);
+        }
+    }
+
+    /**
+     * 通过反射的方式获取状态栏高度
+     * @return
+     */
+    private int getStatusBarHeight() {
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            return getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
