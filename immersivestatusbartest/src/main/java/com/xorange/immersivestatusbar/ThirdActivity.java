92bcd623dd985f769d80d15e77b4a71ce823ba6d
koinhug@126.com <koinhug@126.com>
2016-10-30 13:27:58 +0800

    使用 SystemBarTint 库

--- immersivestatusbartest/src/main/java/com/xorange/immersivestatusbar/ThirdActivity.java
+++ immersivestatusbartest/src/main/java/com/xorange/immersivestatusbar/ThirdActivity.java
@@ -1,7 +1,31 @@
 package com.xorange.immersivestatusbar;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 
 public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            // 激活状态栏
+            tintManager.setStatusBarTintEnabled(true);
+            // 激活导航栏
+            tintManager.setNavigationBarTintEnabled(true);
+            // 给状态栏设置颜色
+            tintManager.setStatusBarTintResource(R.color.mask_tags_1);
+            // 给导航栏设置颜色
+            tintManager.setNavigationBarTintResource(R.color.mask_tags_1);
+        }
+    }
 }
